369a25430279bea43c075f9299479d20d4e61f9f
tigadika <amardika.noor@gmail.com>
2023-08-13 22:10:46 +0700

    start

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const { Movie } = require('../models');
+
+class Controller {
+  static getAllMovies(req, res) { }
+
+  static getOneMovie(req, res) { }
+
+  static async postMovie(req, res) { }
+
+  static async deleteOneMovie(req, res) { }
+}
+
+module.exports = Controller
